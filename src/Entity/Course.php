cd78c6c2467d6a8704cd4c03d6ff2f5a36956517
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-22 23:39:32 -0300

    Create feature for courses
    Create feature for courses, including the listing and the entity
    responsible to manage them inside the system and the database.

---
+++ src/Entity/Course.php
@@ -0,0 +1,41 @@
+<?php
+namespace Alura\Courses\Entity;
+
+/**
+ * @Entity
+ * @Table(name="courses")
+ */
+class Course 
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private $id;
+    /**
+     * @Column(type="string")
+     */
+    private $description;
+
+    public function getId(): int
+    {
+         return $this->id;
+    }
+   
+    public function setId(int $id): voud
+    {
+        $this->id = $id;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): voud
+    {
+        $this->description = $description;
+    }
+}
+
