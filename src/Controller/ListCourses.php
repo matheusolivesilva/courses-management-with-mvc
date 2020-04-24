c18d2e6923891adf7edbfc11f4a58318819a5865
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-24 10:57:34 -0300

    Refactor controllers of course, require views and load

--- src/Controller/ListCourses.php
+++ src/Controller/ListCourses.php
@@ -16,32 +16,6 @@ class ListCourses implements InterfaceRequestController
     public function processRequest(): void
     {
         $courses = $this->repositoryOfCourses->findAll();
-	?>
-
-	<!DOCTYPE html>
-	<html lang="en-us">
-	<head>
-	    <meta charset="utf-8">
-	    <title>Document</title>
-	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
-	<body>
-	<div class="container">
-	    <div class="jumbotron">
-		<h1>List Courses</h1>
-	    </div>
-	    <a href="new-course" class="btn btn-primary mb-2">New Course</a>
-	    <ul class="list-group">
-		<?php foreach ($courses as $course): ?>
-		    <li class="list-group-item">
-			<?= $course->getDescription(); ?>
-		    </li>
-		<?php endforeach; ?>
-	    </ul>
-	</div>
-
-	</body>
-	</html>
-        <?php
+	require __DIR__ . '/../../view/courses/list-courses.php';
     }
 }
