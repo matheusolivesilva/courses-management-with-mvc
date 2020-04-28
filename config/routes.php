569885797979bc69b4dd4016163f932508725c70
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-27 21:22:12 -0300

    Create route that redirects to login page

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,13 @@
 <?php
 
-use Alura\Courses\Controller\{InsertionForm, ListCourses,Persistence, Delete, UpdateForm};
+use Alura\Courses\Controller\{InsertionForm, ListCourses,Persistence, Delete, UpdateFormm, LoginForm};
 
 return [
     '/list-courses' => ListCourses::class,
     '/new-course' => InsertionForm::class,
     '/save-course' => Persistence::class,
     '/delete-course' => Delete::class,
-    '/update-course' => UpdateForm::class
+    '/update-course' => UpdateForm::class,
+    '/login' => LoginForm::class
 ];
 
