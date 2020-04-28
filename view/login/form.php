7154aafccb7547bcf9d6ebf6fdee9d1447b4ea4b
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-27 21:19:25 -0300

    Create view to login

---
+++ view/login/form.php
@@ -0,0 +1,18 @@
+<?php require __DIR__ . '/../header.php'; ?>
+
+    <form action="do-login" method="post">
+        <div class="form-group">
+            <label for="email">Email</label>
+	    <input type="email" name="email" id="email" class="form-control">
+        </div>
+
+	<div class="form-group">
+	    <label for="password">Senha</label>
+	    <input type="password" name="password" id="password" class="form-control">
+	</div>
+	<button class="btn btn-primary">
+	    Log In
+	</button>
+    </form>
+
+<?php require __DIR__ . '/../footer.php'; ?>
